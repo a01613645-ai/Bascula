36ebb04e17449eae7119d80c62d4409e6cf9ee0f
a01613645-ai <a01613645@tec.mx>
2026-04-24 08:15:05 -0600

    Add files via upload

---
+++ ods_salud.py
@@ -0,0 +1,54 @@
+# Importar librerías necearias
+import numpy as np
+import streamlit as st
+import pandas as pd
+
+# Insertamos título
+st.write(''' # ODS 3: Salud y bienestar ''')
+# Insertamos texto con formato
+st.markdown("""
+Esta aplicación utiliza **Machine Learning** para predecir la relacion de las horas de ejercicio realizadas y como 
+afecta en el índice de masa corporal, alineado con el **ODS 3: Salud y bienestar**.
+""")
+# Insertamos una imagen
+st.image("corales.jpg", caption="Horas de ejercicio e índice de masa corporal.")
+
+#st.header('Datos personales')
+
+# Definimos cómo ingresará los datos el usuario
+# Usaremos un deslizador
+st.sidebar.header("Parámetros Ambientales")
+# Definimos los parámetros de nuestro deslizador:
+  # Límite inferior: 24°C. Es el límite inferior donde los arrecifes tropicales suelen estar cómodos
+  # Límite superior: 35°C. La mayoría de los corales mueren o se blanquean totalmente mucho antes de llegar a esa temperatura
+  # Valor inicial: 28°C. En muchos arrecifes, a partir de los 28.5°C o 29°C comienza el estrés térmico severo
+temp_input = st.sidebar.slider("horas trabajadas ", 1.0, 2.0, 3.0)
+
+# Cargamos el archivo con los datos (.csv)
+df =  pd.read_csv('dataset_ods3_regresion_lineal_correlacion_alta.csv', encoding='latin-1')
+# Seleccionamos las variables
+X = df[['horas_ejercicio_semana']]
+y = df['indice_masa_corporal']
+
+# Creamos y entrenamos el modelo
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+LR = LinearRegression()
+LR.fit(X_train,y_train)
+
+# Hacemos la predicción con el modelo y la temperatura seleccionada por el usuario
+b1 = LR.coef_
+b0 = LR.intercept_
+prediccion = b0 + b1[0]*temp_input
+
+# Presentamos loa resultados
+st.subheader('predicción de índice de masa corporal (IMC)')
+st.write(f'El IMC estimado es : {prediccion:.2f}%')
+
+if prediccion < 18.5:
+        st.success("Estado: peso normal")
+elif prediccion < 25:
+        st.warning("Estado: Sobrepeso")
+else:
+        st.error("obesidad")
